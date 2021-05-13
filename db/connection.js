09b1f4632f5b714724945536c23f545184a0bd92
powerar <arpower@bell.net>
2021-05-13 18:52:59 -0400

    obfuscated pw, user, db

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 //Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      user: 'root',
-      password: 'M9&8z9@#GTYB',
-      database: 'employees',
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
     },
     console.log('Connected to the election database.')
   );
